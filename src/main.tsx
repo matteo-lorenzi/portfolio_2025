b675e2ca860d29f2b87b343890dd2a64495fb76b
Lolopiz <[lorenzi.matteo30@gmail.com]>
2025-07-06 16:37:21 +0200

    fix: set basename for BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio_2025/">
       <App />
     </BrowserRouter>
   </StrictMode>,
